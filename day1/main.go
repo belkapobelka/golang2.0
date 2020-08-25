9577703625d4da5fd8b6d7a9264c644c1f086fa4
belkapobelka <nat52107499@yandex.ru>
2020-08-25 19:48:01 +0300

    наброски

--- day1/main.go
+++ day1/main.go
@@ -1,1 +1,75 @@
 package main
+
+import (
+	_ "database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+)
+
+type Solution struct {
+	A      float64 `json:"a"`
+	B      float64 `json:"b"`
+	C      float64 `json:"c"`
+	NRoots int     `json:"n_roots"`
+}
+
+var solutions []Solution
+
+func main() {
+	fmt.Println("API was started")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/solve/{a}/{b}/{c}", AddSolution).Methods("POST")
+	router.HandleFunc("/solution", GetLastSolution).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func AddSolution(writer http.ResponseWriter, request *http.Request) {
+	var solution Solution
+	reqBody := mux.Vars(request)
+	solution.A, _ = strconv.ParseFloat(reqBody["a"], 64)
+	solution.B, _ = strconv.ParseFloat(reqBody["b"], 64)
+	solution.C, _ = strconv.ParseFloat(reqBody["c"], 64)
+
+	CountNRoots(&solution)
+	solutions = append(solutions, solution)
+	writer.WriteHeader(http.StatusCreated)
+}
+
+func CountNRoots(s *Solution) {
+	if (s.A == 0 && s.B != 0) || (s.A != 0 && s.C == 0 && s.B == 0) || (s.A == s.B && s.C == 0) {
+		s.NRoots = 1
+		return
+	}
+	if s.A == 0 && s.B == 0 {
+		s.NRoots = 0
+		return
+	}
+
+	discr := math.Pow(s.B, 2) - 4*s.A*s.C
+	x1 := (-s.B + math.Sqrt(discr)) / (2 * s.A)
+	x2 := (-s.B - math.Sqrt(discr)) / (2 * s.A)
+	if discr == 0 || x1 == x2 {
+		s.NRoots = 1
+	} else if discr > 0 {
+		s.NRoots = 2
+	} else {
+		s.NRoots = 0
+	}
+}
+
+func GetLastSolution(writer http.ResponseWriter, request *http.Request) {
+	if len(solutions) == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(writer).Encode("Ничего не нашли")
+	} else {
+		json.NewEncoder(writer).Encode(solutions[len(solutions)-1])
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module golang2.0
 
 go 1.15
+
+require (
+	github.com/go-pg/pg v8.0.7+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.1.1
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/onsi/ginkgo v1.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	mellium.im/sasl v0.2.1 // indirect
+)
